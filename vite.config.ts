a277087ec31b0b833b35dffc7df8ed0d8e3091b5
assacy-cmyk <assacy@gmail.com>
2026-02-22 15:08:45 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
